3f9bba1f36934986477f53a924ab625a3ad25d1a
mmongan <41438677+mmongan@users.noreply.github.com>
2024-01-17 18:34:04 -0600

    add world

---
+++ src/world.ts
@@ -0,0 +1,11 @@
+
+import * as BABYLON from 'babylonjs'
+
+export function setupWorld(canvas: HTMLCanvasElement) {  
+    
+  const engine = new BABYLON.Engine(canvas, true);
+
+  console.log(engine.version);
+  
+  // code continues....}
+}
