253d4e1d23133a8ee5bc5f3faf780cba7ec2f5f6
mmongan <marty.mongan@gmail.com>
2026-08-01 16:12:50 -0500

    deploy

--- assets/tgaTextureLoader-DeubNTgd.js
+++
@@ -1,2 +0,0 @@
-import{h as r,i as n}from"./index-B8fZj2q3.js";class i{constructor(){this.supportCascades=!1}loadCubeData(){throw".env not supported in Cube."}loadData(e,t,o){const s=new Uint8Array(e.buffer,e.byteOffset,e.byteLength),a=r(s);o(a.width,a.height,t.generateMipMaps,!1,()=>{n(t,s)})}}export{i as _TGATextureLoader};
-//# sourceMappingURL=tgaTextureLoader-DeubNTgd.js.map
