0eb5965f6111aeae748e5fad54d75c2dab757c56
mmongan <marty.mongan@gmail.com>
2026-08-01 06:01:10 -0500

    deploy

---
+++ assets/tgaTextureLoader-B7v-SMWH.js
@@ -0,0 +1,2 @@
+import{h as r,i as n}from"./index-DWlU5vpS.js";class i{constructor(){this.supportCascades=!1}loadCubeData(){throw".env not supported in Cube."}loadData(e,t,o){const s=new Uint8Array(e.buffer,e.byteOffset,e.byteLength),a=r(s);o(a.width,a.height,t.generateMipMaps,!1,()=>{n(t,s)})}}export{i as _TGATextureLoader};
+//# sourceMappingURL=tgaTextureLoader-B7v-SMWH.js.map
