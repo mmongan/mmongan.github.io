ea1ef4045bea25b162d523bb5de67fcd1f40db27
mmongan <marty.mongan@gmail.com>
2026-08-04 23:08:52 -0500

    deploy

---
+++ assets/tgaTextureLoader-Cg25RLjh.js
@@ -0,0 +1,2 @@
+import{h as r,i as n}from"./index-EEWhL8Zn.js";class i{constructor(){this.supportCascades=!1}loadCubeData(){throw".env not supported in Cube."}loadData(e,t,o){const s=new Uint8Array(e.buffer,e.byteOffset,e.byteLength),a=r(s);o(a.width,a.height,t.generateMipMaps,!1,()=>{n(t,s)})}}export{i as _TGATextureLoader};
+//# sourceMappingURL=tgaTextureLoader-Cg25RLjh.js.map
