d5ec77b2b39bb5bb49141da7981d778ee9b3494c
mmongan <marty.mongan@gmail.com>
2026-08-06 18:38:05 -0500

    deploy

--- assets/tgaTextureLoader-9W19FPH-.js
+++
@@ -1,2 +0,0 @@
-import{h as r,i as n}from"./index-ByqM9xLZ.js";class i{constructor(){this.supportCascades=!1}loadCubeData(){throw".env not supported in Cube."}loadData(e,t,o){const s=new Uint8Array(e.buffer,e.byteOffset,e.byteLength),a=r(s);o(a.width,a.height,t.generateMipMaps,!1,()=>{n(t,s)})}}export{i as _TGATextureLoader};
-//# sourceMappingURL=tgaTextureLoader-9W19FPH-.js.map
