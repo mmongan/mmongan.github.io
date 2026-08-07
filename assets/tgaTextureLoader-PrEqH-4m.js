8ddec6e6ea4b04b4a632a8c3b875cabec06cb3f8
mmongan <marty.mongan@gmail.com>
2026-08-07 11:43:18 -0500

    deploy

---
+++ assets/tgaTextureLoader-PrEqH-4m.js
@@ -0,0 +1,2 @@
+import{h as r,i as n}from"./index-CnVYr0rk.js";class i{constructor(){this.supportCascades=!1}loadCubeData(){throw".env not supported in Cube."}loadData(e,t,o){const s=new Uint8Array(e.buffer,e.byteOffset,e.byteLength),a=r(s);o(a.width,a.height,t.generateMipMaps,!1,()=>{n(t,s)})}}export{i as _TGATextureLoader};
+//# sourceMappingURL=tgaTextureLoader-PrEqH-4m.js.map
