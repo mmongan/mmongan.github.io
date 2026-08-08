1c40f50c7ab99ea23d98274bc737e5823ccf9d49
mmongan <marty.mongan@gmail.com>
2026-08-08 18:34:41 -0500

    deploy

--- assets/flowGraphGLTFDataProvider-CEIW0bYr.js
+++
@@ -1,2 +0,0 @@
-import{aE as i,aF as t}from"./package-5MT41zm-.js";class m extends i{constructor(s){super();const o=s.glTF,r=o.animations?.map(a=>a._babylonAnimationGroup)||[];this.animationGroups=this.registerDataOutput("animationGroups",t,r);const n=o.nodes?.map(a=>a._babylonTransformNode)||[];this.nodes=this.registerDataOutput("nodes",t,n)}getClassName(){return"FlowGraphGLTFDataProvider"}}export{m as FlowGraphGLTFDataProvider};
-//# sourceMappingURL=flowGraphGLTFDataProvider-CEIW0bYr.js.map
---
+++ assets/tgaTextureLoader-B51crY5d.js
@@ -0,0 +1,2 @@
+import{aR as r,aS as n}from"./package-DDk5QU78.js";class f{constructor(){this.supportCascades=!1}loadCubeData(){throw".env not supported in Cube."}loadData(e,t,o){const a=new Uint8Array(e.buffer,e.byteOffset,e.byteLength),s=r(a);o(s.width,s.height,t.generateMipMaps,!1,()=>{n(t,a)})}}export{f as _TGATextureLoader};
+//# sourceMappingURL=tgaTextureLoader-B51crY5d.js.map
