59ea08b6ee3ddb178f9eecd4a6fd16b762f9f98b
mmongan <marty.mongan@gmail.com>
2026-08-08 10:40:34 -0500

    deploy

--- assets/tgaTextureLoader-BUqkv_kT.js
+++
@@ -1,2 +0,0 @@
-import{h as r,i as n}from"./index-DzAAh7qm.js";class i{constructor(){this.supportCascades=!1}loadCubeData(){throw".env not supported in Cube."}loadData(e,t,o){const s=new Uint8Array(e.buffer,e.byteOffset,e.byteLength),a=r(s);o(a.width,a.height,t.generateMipMaps,!1,()=>{n(t,s)})}}export{i as _TGATextureLoader};
-//# sourceMappingURL=tgaTextureLoader-BUqkv_kT.js.map
