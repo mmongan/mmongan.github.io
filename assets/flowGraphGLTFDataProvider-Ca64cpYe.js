e4d171fd25d8be17dc510c55a915e405dfec7ee0
mmongan <marty.mongan@gmail.com>
2026-08-07 20:04:16 -0500

    deploy

---
+++ assets/flowGraphGLTFDataProvider-Ca64cpYe.js
@@ -0,0 +1,2 @@
+import{F as i,R as t}from"./index-lqW8dpXx.js";class m extends i{constructor(s){super();const a=s.glTF,r=a.animations?.map(o=>o._babylonAnimationGroup)||[];this.animationGroups=this.registerDataOutput("animationGroups",t,r);const n=a.nodes?.map(o=>o._babylonTransformNode)||[];this.nodes=this.registerDataOutput("nodes",t,n)}getClassName(){return"FlowGraphGLTFDataProvider"}}export{m as FlowGraphGLTFDataProvider};
+//# sourceMappingURL=flowGraphGLTFDataProvider-Ca64cpYe.js.map
