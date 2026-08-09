4a484944354d12eceb66823b6fd9720bc7f98377
mmongan <marty.mongan@gmail.com>
2026-08-09 17:38:51 -0500

    deploy

--- assets/tgaTextureLoader-CSpawu_h.js
+++
@@ -1,2 +0,0 @@
-import{a6 as r,a7 as n}from"./package-B6yiike9.js";class f{constructor(){this.supportCascades=!1}loadCubeData(){throw".env not supported in Cube."}loadData(e,t,o){const a=new Uint8Array(e.buffer,e.byteOffset,e.byteLength),s=r(a);o(s.width,s.height,t.generateMipMaps,!1,()=>{n(t,a)})}}export{f as _TGATextureLoader};
-//# sourceMappingURL=tgaTextureLoader-CSpawu_h.js.map
