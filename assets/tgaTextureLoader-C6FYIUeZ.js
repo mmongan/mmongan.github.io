e7df3507a2d66d22c3a7e16b0e92033c89e002a8
mmongan <marty.mongan@gmail.com>
2026-08-09 21:35:49 -0500

    deploy

---
+++ assets/tgaTextureLoader-C6FYIUeZ.js
@@ -0,0 +1,2 @@
+import{a5 as r,a6 as n}from"./package-BoiDK--I.js";class f{constructor(){this.supportCascades=!1}loadCubeData(){throw".env not supported in Cube."}loadData(e,t,o){const a=new Uint8Array(e.buffer,e.byteOffset,e.byteLength),s=r(a);o(s.width,s.height,t.generateMipMaps,!1,()=>{n(t,a)})}}export{f as _TGATextureLoader};
+//# sourceMappingURL=tgaTextureLoader-C6FYIUeZ.js.map
