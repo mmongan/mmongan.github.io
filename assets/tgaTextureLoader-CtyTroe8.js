14ae75a8a0669874b5ff5ff2fc67426d77554b89
mmongan <marty.mongan@gmail.com>
2026-08-09 21:34:19 -0500

    deploy

---
+++ assets/tgaTextureLoader-CtyTroe8.js
@@ -0,0 +1,2 @@
+import{a5 as r,a6 as n}from"./package-bBrBgyPN.js";class f{constructor(){this.supportCascades=!1}loadCubeData(){throw".env not supported in Cube."}loadData(e,t,o){const a=new Uint8Array(e.buffer,e.byteOffset,e.byteLength),s=r(a);o(s.width,s.height,t.generateMipMaps,!1,()=>{n(t,a)})}}export{f as _TGATextureLoader};
+//# sourceMappingURL=tgaTextureLoader-CtyTroe8.js.map
